f7519aa8ba15ddb2707afd5c47323372c6afd774
David Brun <brundavid@gmail.com>
2012-01-03 14:22:46 +0100

    Add a property to store the DownloadManager
    Signed-off-by: David Brun <brundavid@gmail.com>

--- src/fr/uha/ensisa/jadoma/controller/ControllerFrmMain.java
+++ src/fr/uha/ensisa/jadoma/controller/ControllerFrmMain.java
@@ -1,12 +1,15 @@
 package fr.uha.ensisa.jadoma.controller;
 
+import fr.uha.ensisa.jadoma.model.DownloadManager;
 import fr.uha.ensisa.jadoma.view.FrmMain;
 
 public class ControllerFrmMain {
 	
 	private FrmMain frmMain;
+	private DownloadManager downloadManager;
 	
 	public ControllerFrmMain(FrmMain frmMain) {
 		this.frmMain = frmMain;
+		this.downloadManager = new DownloadManager();
 	}
 }
