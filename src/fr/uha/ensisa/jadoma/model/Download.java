175da1466d1aa46ec53f613ab55b5074a4c39cad
David Brun <brundavid@gmail.com>
2012-01-03 14:08:08 +0100

    Change the Download class to abstract Add 2 subclasses UniPartDownload and MultiPartDownload Begin of the implementation: add a property to store the parts

--- src/fr/uha/ensisa/jadoma/model/Download.java
+++ src/fr/uha/ensisa/jadoma/model/Download.java
@@ -1,5 +1,17 @@
 package fr.uha.ensisa.jadoma.model;
 
-public class Download {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Download {
+	
+	protected List<DownloadPart> listDownloadParts;
+	
+	public Download() {
+		this.listDownloadParts = new ArrayList<DownloadPart>();
+	}
+	
+	public List<DownloadPart> getListDownloadParts() {
+		return listDownloadParts;
+	}
 }
