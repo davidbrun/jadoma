4958a9e1539fa27ceb6df1e8a35da8838a58d0ef
David Brun <brundavid@gmail.com>
2012-01-03 21:29:52 +0100

    Implement DownloadPart functionalities
    Signed-off-by: David Brun <brundavid@gmail.com>

--- src/fr/uha/ensisa/jadoma/model/DownloadPart.java
+++ src/fr/uha/ensisa/jadoma/model/DownloadPart.java
@@ -1,5 +1,36 @@
 package fr.uha.ensisa.jadoma.model;
 
 public abstract class DownloadPart {
+	
+	private int beginByte;
+	private int endByte;
+	
+	public DownloadPart(int beginByte, int endByte) {
+		this.beginByte = beginByte;
+		this.endByte = endByte;
+	}
 
+	public int getBeginByte() {
+		return beginByte;
+	}
+
+	public void setBeginByte(int beginByte) {
+		this.beginByte = beginByte;
+	}
+
+	public int getEndByte() {
+		return endByte;
+	}
+
+	public void setEndByte(int endByte) {
+		this.endByte = endByte;
+	}
+	
+	public abstract double getPercentCompleted();
+	
+	public abstract int getNbrOfCompletedBytes();
+	
+	public int getDownloadPartSize() {
+		return this.endByte - this.beginByte;
+	}
 }
--- src/fr/uha/ensisa/jadoma/model/EmptyDownloadPart.java
+++ src/fr/uha/ensisa/jadoma/model/EmptyDownloadPart.java
@@ -2,4 +2,17 @@ package fr.uha.ensisa.jadoma.model;
 
 public class EmptyDownloadPart extends DownloadPart {
 
+	public EmptyDownloadPart(int beginByte, int endByte) {
+		super(beginByte, endByte);
+	}
+
+	@Override
+	public double getPercentCompleted() {
+		return 0.0;
+	}
+
+	@Override
+	public int getNbrOfCompletedBytes() {
+		return 0;
+	}
 }
