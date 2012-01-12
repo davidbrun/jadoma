598135bdd0f907e10fbb4642fd8e6adf86351761
Théophile Helleboid - chtitux <chtitux@gmail.com>
2012-01-12 20:14:30 +0100

    Fix unused import

--- src/fr/uha/ensisa/jadoma/view/FrmMain.java
+++ src/fr/uha/ensisa/jadoma/view/FrmMain.java
@@ -16,7 +16,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
 
 import fr.uha.ensisa.jadoma.controller.ControllerFrmMain;
