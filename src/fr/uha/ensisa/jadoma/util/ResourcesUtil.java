f4ff2af4bbf03969863a92e197897324702313a8
David Brun <brundavid@gmail.com>
2012-02-16 17:22:58 +0100

    Modify an icon to have the correct dimension Modify the class path to be able to generate easily a runnable jar Modify the ResourcesUtil to be able to load resources in the jar
    Signed-off-by: David Brun <brundavid@gmail.com>

--- src/fr/uha/ensisa/jadoma/util/ResourcesUtil.java
+++ src/fr/uha/ensisa/jadoma/util/ResourcesUtil.java
@@ -1,7 +1,5 @@
 package fr.uha.ensisa.jadoma.util;
 
-import java.io.File;
-import java.net.URL;
 import javax.swing.ImageIcon;
 
 /**
@@ -53,68 +51,25 @@ public class ResourcesUtil
     
     static
     {
-        RESOURCES_PATH = new File(fr.uha.ensisa.jadoma.util.ResourcesUtil.class.getResource("/").getPath()).getParent() + "/resources";
+        RESOURCES_PATH = "resources";
         
-//        Toolkit kit = Toolkit.getDefaultToolkit();
-        // Locate the resources
-//        URL urlApplicationImage = null;
-        URL urlStartButtonImage = null;
-        URL urlStopButtonImage = null;
-        URL urlPauseButtonImage = null;
-        
-        URL urlAddDownloadButtonImage = null;
-        URL urlStartDownloadButtonImage = null;
-        URL urlPauseDownloadButtonImage = null;
-        URL urlCancelDownloadButtonImage = null;
-        URL urlAddDownloadsButtonImage = null;
-        URL urlStartDownloadsButtonImage = null;
-        URL urlPauseDownloadsButtonImage = null;
-        URL urlCancelDownloadsButtonImage = null;
-        URL urlPreferencesButtonImage = null;
-        URL urlSchedulerButtonImage = null;
-        URL urlAboutBoxImage = null;
-//        URL urlMarkerFeltFont = null;
-        
-        try
-        {
-//            urlApplicationImage = new URL("file:" + RESOURCES_PATH + "/images/");
-            urlStartButtonImage = new URL("file:" + RESOURCES_PATH + "/images/play_24x24.png");
-            urlStopButtonImage = new URL("file:" + RESOURCES_PATH + "/images/cancel_24x24.png");
-            urlPauseButtonImage = new URL("file:" + RESOURCES_PATH + "/images/pause_24x24.png");
-            
-            urlAddDownloadButtonImage = new URL("file:" + RESOURCES_PATH + "/images/add_32x32.png");;
-            urlStartDownloadButtonImage = new URL("file:" + RESOURCES_PATH + "/images/play_32x32.png");;
-            urlPauseDownloadButtonImage = new URL("file:" + RESOURCES_PATH + "/images/stop_32x32.png");;
-            urlCancelDownloadButtonImage = new URL("file:" + RESOURCES_PATH + "/images/cancel_32x32.png");;
-            urlAddDownloadsButtonImage = new URL("file:" + RESOURCES_PATH + "/images/add-all_32x32.png");;
-            urlStartDownloadsButtonImage = new URL("file:" + RESOURCES_PATH + "/images/play-all_32x32.png");;
-            urlPauseDownloadsButtonImage = new URL("file:" + RESOURCES_PATH + "/images/pause-all_32x32.png");;
-            urlCancelDownloadsButtonImage = new URL("file:" + RESOURCES_PATH + "/images/cancel-all_32x32.png");;
-            urlPreferencesButtonImage = new URL("file:" + RESOURCES_PATH + "/images/settings_32x32.png");;
-            urlSchedulerButtonImage = new URL("file:" + RESOURCES_PATH + "/images/scheduler_32x32.png");;
-            urlAboutBoxImage = new URL("file:" + RESOURCES_PATH + "/images/help_32x32.png");
-//            urlMarkerFeltFont = new URL("file:" + RESOURCES_PATH + "/fonts/MarkerFelt.pfb");
-        }
-        catch (Exception e)
-        { }
-        finally
-        {
-//            APPLICATION_IMAGE = kit.createImage(urlApplicationImage);
-        	START_BUTTON_IMAGE_ICON = new ImageIcon(urlStartButtonImage);
-        	STOP_BUTTON_IMAGE_ICON = new ImageIcon(urlStopButtonImage);
-        	PAUSE_BUTTON_IMAGE_ICON = new ImageIcon(urlPauseButtonImage);
-        	TOOLBAR_START_BUTTON_IMAGE_ICON = new ImageIcon(urlStartDownloadButtonImage);
-            TOOLBAR_PAUSE_BUTTON_IMAGE_ICON = new ImageIcon(urlPauseDownloadButtonImage);
-            TOOLBAR_CANCEL_BUTTON_IMAGE_ICON = new ImageIcon(urlCancelDownloadButtonImage);
-            TOOLBAR_ADD_BUTTON_IMAGE_ICON = new ImageIcon(urlAddDownloadButtonImage);
-            TOOLBAR_ADD_LIST_BUTTON_IMAGE_ICON = new ImageIcon(urlAddDownloadsButtonImage);
-            TOOLBAR_START_ALL_BUTTON_IMAGE_ICON = new ImageIcon(urlStartDownloadsButtonImage);
-            TOOLBAR_PAUSE_ALL_BUTTON_IMAGE_ICON = new ImageIcon(urlPauseDownloadsButtonImage);
-            TOOLBAR_CANCEL_ALL_BUTTON_IMAGE_ICON = new ImageIcon(urlCancelDownloadsButtonImage);
-            TOOLBAR_PREFERENCES_BUTTON_IMAGE_ICON = new ImageIcon(urlPreferencesButtonImage);
-            TOOLBAR_SCHEDULER_BUTTON_IMAGE_ICON = new ImageIcon(urlSchedulerButtonImage);
-            ABOUT_BOX_IMAGE_ICON = new ImageIcon(urlAboutBoxImage);
-//            MARKER_FELT_FONT = urlMarkerFeltFont;
-        }
+//		Toolkit kit = Toolkit.getDefaultToolkit();
+		// Load the resources
+//		APPLICATION_IMAGE = kit.createImage(RESOURCES_PATH + "/images/");
+		START_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/play_24x24.png");
+		STOP_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/cancel_24x24.png");
+		PAUSE_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/pause_24x24.png");
+		TOOLBAR_START_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/play_32x32.png");
+		TOOLBAR_PAUSE_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/stop_32x32.png");
+		TOOLBAR_CANCEL_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/cancel_32x32.png");
+		TOOLBAR_ADD_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/add_32x32.png");
+		TOOLBAR_ADD_LIST_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/add-all_32x32.png");
+		TOOLBAR_START_ALL_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/play-all_32x32.png");
+		TOOLBAR_PAUSE_ALL_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/pause-all_32x32.png");
+		TOOLBAR_CANCEL_ALL_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/cancel-all_32x32.png");
+		TOOLBAR_PREFERENCES_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/settings_32x32.png");
+		TOOLBAR_SCHEDULER_BUTTON_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/scheduler_32x32.png");
+		ABOUT_BOX_IMAGE_ICON = new ImageIcon(RESOURCES_PATH + "/images/help_32x32.png");
+//		MARKER_FELT_FONT = RESOURCES_PATH + "/fonts/MarkerFelt.pfb";
     }
 }
